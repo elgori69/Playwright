4ebcc8502a9e6848961acf9ccefc3f0fe89d0e68
elgori69 <42331777+elgori69@users.noreply.github.com>
2025-01-28 00:10:00 -0600

    Add files via upload

---
+++ playwright.config.js
@@ -0,0 +1,18 @@
+// @ts-check
+const { devices } = require('@playwright/test');
+
+const config = {
+  testDir: './tests',
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000
+  },
+  
+  reporter: 'html',
+  use: {
+    browserName : 'chromium',
+    headless : false
+  }
+}
+module.exports = config;
